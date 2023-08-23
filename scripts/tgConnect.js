182ba414258f7191bd0ab4a1eb146f6c31ee4293
ReisG <romanryabcev2006@gmail.com>
2023-08-23 10:34:54 +0300

    taking information about phisic user

--- scripts/tgConnect.js
+++ scripts/tgConnect.js
@@ -5,11 +5,27 @@ function sendData()
     let data = {};
     if (MODE == "phis")
     {
+        // reading data and validating it
 
+        // parsing passport data
+        let passport = document.getElementById("passport").value;
+        let passport_series = Number(passport.split(" ")[0]);
+        let passport_number = Number(passport.split(" ")[1]);
+
+        data = {
+            "first_name" : document.getElementById("first_name").value,
+            "last_name" : document.getElementById("last_name").value,
+            "fathers_name" : document.getElementById("fathers_name").value,
+            "passport_series" : passport_series,
+            "passport_number" : passport_number, 
+            "address" : document.getElementById("address"),
+            "inn" : Number(document.getElementById("inn").value),
+            "phone_number" : document.getElementById("phone_number")
+        };
     }
     else if (MODE == "urid")
     {
-
+        
     }
 
     return data;
